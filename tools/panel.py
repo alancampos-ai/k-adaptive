df89fe9db95ef8f23bd2d208f0a560cdbb1592ba
Alan Campos <alan.campos@pos.ime.uerj.br>
2025-10-17 21:20:20 -0300

    panel.py

--- tools/panel.py
+++ tools/panel.py
@@ -3,7 +3,6 @@ import json
 import argparse
 from typing import List, Tuple
 from PIL import Image, ImageDraw, ImageFont
-
 def load_font(pt: int) -> ImageFont.FreeTypeFont:
     try:
         return ImageFont.truetype("DejaVuSans.ttf", pt)
