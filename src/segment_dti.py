cd3f6ca4bd0067920775668712c83f11a556090c
Alan Campos <alan.campos@pos.ime.uerj.br>
2025-10-27 18:42:23 -0300

    Update segment_dti.py

--- src/segment_dti.py
+++ src/segment_dti.py
@@ -1,28 +1,30 @@
-import numpy as np
-from .kmeans import KMeans, Point
+import numpy as np 
+from gmm import GMM, Point
 
-def segmentation(dti, n_claster, mask=None, metric_type='airm', expoent=1,
-                 index_centers=None, max_iterations=1000, dim_point=3):
+def segmentation(dti, n_claster, mask=None, metric_type='riemannian',
+                 expoent=1.0, index_centers=None, max_iterations=1000, dim_point=3):
     if mask is None:
-        mask = np.ones(dti.shape[0:3], dtype=np.bool_)
-    images = np.zeros(mask.shape, dtype=np.int16)
+        mask = np.ones(dti.shape[:3], dtype=np.bool_)
+    else:
+        mask = mask.astype(np.bool_)
+
+    total = int(np.prod(dti.shape[:3]))
+    mask_flat = mask.reshape(-1)
+    dti_flat = dti.reshape(total, 3, 3)
+
     points = []
-    total_points = np.prod(dti.shape[0:3])
-    mask_seg = np.reshape(mask, (total_points,))
-    DTI = np.reshape(dti, (total_points, 3, 3))
-    imagem = np.zeros((total_points,), dtype=np.int16)
-    for i, value in enumerate(DTI):
-        if mask_seg[i]:
-            if value.any():
-                point = Point(value, i)
-                points.append(point)
-    total_points_mask = len(points)
-    kmeans = KMeans(n_claster=n_claster, metric_type=metric_type,
-                    index_centers=index_centers, total_points=total_points_mask,
+    for i, M in enumerate(dti_flat):
+        if mask_flat[i] and np.any(M):
+            points.append(Point(M, i))
+
+    gmm = GMM(n_claster=n_claster, metric_type=metric_type,
+                    index_centers=index_centers, total_points=len(points),
                     max_iterations=max_iterations, dim_point=dim_point)
-    classes = kmeans.fit(points, expoent=expoent)
-    imagem_idx = np.array([i for i in sorted(classes)])
-    imagem_ = np.array([classes[i] for i in sorted(classes)], dtype=np.int16)
-    imagem[imagem_idx] = imagem_
-    images = np.reshape(imagem, (images.shape[0], images.shape[1], images.shape[2]))
-    return images
+    classes = gmm.fit(points, expoent=expoent)  
+
+    y = np.zeros(total, dtype=np.int32)
+    if classes:
+        idx = np.fromiter(sorted(classes.keys()), dtype=np.int64)
+        lbl = np.fromiter((classes[i] for i in idx), dtype=np.int32)
+        y[idx] = lbl
+    return y.reshape(mask.shape)
