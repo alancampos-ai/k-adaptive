6bf7010d756169da94c89ec20076551f4a3b6a07
Alan Campos <alan.campos@pos.ime.uerj.br>
2025-12-07 18:56:08 -0300

    Atualizar o heatmaps.py

--- tools/heatmaps.py
+++ tools/heatmaps.py
@@ -1,7 +1,7 @@
 import os    
 import json   
 from pathlib import Path 
-import numpy as np
+import numpy as np 
 import matplotlib.pyplot as plt
 from matplotlib import colors, patches, patheffects
 
