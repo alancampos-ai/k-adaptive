db8bee5d4817314a4357c54ad85bebc21d178b85
Alan Campos <alan.campos@pos.ime.uerj.br>
2025-12-13 16:11:29 -0300

    Atualizar o bars.py

--- tools/bars.py
+++ tools/bars.py
@@ -1,7 +1,7 @@
 from pathlib import Path 
 import json     
 import numpy as np   
-import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt 
 import matplotlib.ticker as mtick  
  
 here = Path(__file__).resolve()
